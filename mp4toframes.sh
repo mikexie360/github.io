bda591fc96484f5b72056caa27169ad89f6de09e
mikexie360 <33299123+mikexie360@users.noreply.github.com>
2019-02-24 11:39:40 -0600

    Update mp4toframes.sh

--- mp4toframes.sh
+++ mp4toframes.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 #This program will convert mp4 files into frames.
 #The frames will appear in another directory
+rm frame_*
 ffmpeg -i $1 -y -an -q 0 -vf fps=10 frame_%03d.png
+rm *.mp4
